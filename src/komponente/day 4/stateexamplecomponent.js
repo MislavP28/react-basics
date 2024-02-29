92b9f71496b5c8f7fc37cd35bbc5e654ef18f78e
MislavP28 <mihapavicic@gmail.com>
2024-02-29 19:14:57 +0100

    push

--- src/komponente/day 4/stateexamplecomponent.js
+++ src/komponente/day 4/stateexamplecomponent.js
@@ -1,10 +1,30 @@
-import { useState } from "react"
+import { useState } from "react";
+import userData from "./userdata";
 
 function StatesExampleComponent(){
     //Starts business logic
+    console.log("user data ==>", userData)
     const initialGreeting = "Hello guest";
+    let initialUser = {
+        name: "Mislav",
+        lastName: "Pavičić",
+        role: "developer",
+        imageUrl: "https://miro.medium.com/v2/resize:fit:800/1*6T76GFF-nAN2GQ0w4Njznw.jpeg"
+    }
+    if(userData !== undefined){initialUser = userData}
     const [greeting, setGreeting] = useState(initialGreeting);
     const [count, setCount] =useState(0);
+    const ButtonFancy = {
+        backgroundColor:"plum",
+        color:"white",
+        margin:"20px",
+        border:"2px solid black",
+        borderRadius:"50px",
+        width:"100px",
+        height:"50px",
+        font:"larger"
+    };
+    const [user, setUser] = useState(initialUser);
 
 
     function handleCounter(){
@@ -21,13 +41,17 @@ function StatesExampleComponent(){
         }
         setCount(count + 1)
     }
+    function handleGreeting(){
+        setGreeting(`Hello ${user.name} ${user.lastName} (${user.role})`)
+    }
 
     //End business logic
     return (
         <div className={"day4-component"}>
             <h3>{greeting} {count} times</h3>
-            <button onClick={() => setGreeting("Hello Mislav")}>Greet me</button>
-            <button onClick={() => setCount(count + 1)}>Count</button>
+            <img src={user.imageUrl}></img>
+            <button style={ButtonFancy} onClick={() => handleGreeting()}>Greet me</button>
+            <button style={ButtonFancy} onClick={() => handleCounter()}>Count</button>
         </div>
     )
 }
---
+++ src/komponente/day 4/userdata.js
@@ -0,0 +1,7 @@
+export const userData = {
+    name: 'Mislav',
+    lastName: "Pavičić",
+    role: 'Developer',
+    imageUrl: "https://miro.medium.com/v2/resize:fit:800/1*6T76GFF-nAN2GQ0w4Njznw.jpeg",
+  };
+  export default userData
